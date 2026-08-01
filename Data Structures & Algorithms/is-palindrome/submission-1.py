fb74aafa5cebf039e6e45b8362916d9840028ff4
Ricardo Pramana Suranta <edopelawi@users.noreply.github.com>
2026-08-02 01:17:31 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+
+        # Alphanumeric filtering + lowercasing
+        alnumString = "".join([c.lower() for c in s if c.isalnum()])
+
+        maxIdx = len(alnumString) // 2
+        # print("[^^^] Alphanumeric String: ", alnumString)
+        # print("[^^^] Max Idx: ", maxIdx)
+        for i in range(maxIdx):
+            left = alnumString[i]
+            right = alnumString[-i-1]
+            if left != right:
+                # print(f"[^^^] Invalid matches, char on {i}: {left}, char on {len(alnumString) - i} (or -{i}): {right}")
+                return False
+        
+        return True
+        
