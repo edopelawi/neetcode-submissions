a1b9e2ba824717c66aca108d734cd74168474322
Ricardo Pramana Suranta <edopelawi@users.noreply.github.com>
2026-08-08 18:06:02 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = None
+        maxProfit = 0
+
+        for idx in range(len(prices)):
+            price = prices[idx]
+
+            if minPrice is None:                
+                minPrice = price
+                continue
+            
+            profit = price - minPrice
+            # print(f"[^^^] idx: {idx}, current minPrice: {minPrice}, current maxProfit: {maxProfit}, new price: {price}")
+            if profit > maxProfit:
+                # print("[^^^^^] new maxProfit unlocked: ", profit)
+                maxProfit = profit
+
+            if minPrice > price:
+                # print("[^^^^^] new minPrice unlocked: ", price)
+                minPrice = price
+
+        
+        return maxProfit
+        
